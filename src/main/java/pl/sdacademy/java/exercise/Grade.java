9659b8bf2ed092dbdf216d13f6bddf42990dce96
szotas <krystian.szota.it@gmail.com>
2022-12-09 17:54:22 +0100

    dodana klasa grade

---
+++ src/main/java/pl/sdacademy/java/exercise/Grade.java
@@ -0,0 +1,61 @@
+package pl.sdacademy.java.exercise;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Grade {
+    private UUID courseUuid;
+    private GradeType gradeType;
+    private LocalDateTime dateTime;
+    private BigDecimal value;
+
+    public UUID getCourseUuid() {
+        return courseUuid;
+    }
+
+    public void setCourseUuid(UUID courseUuid) {
+        this.courseUuid = courseUuid;
+    }
+
+    public GradeType getGradeType() {
+        return gradeType;
+    }
+
+    public void setGradeType(GradeType gradeType) {
+        this.gradeType = gradeType;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public Grade(UUID courseUuid, GradeType gradeType, LocalDateTime dateTime, BigDecimal value) {
+        this.courseUuid = courseUuid;
+        this.gradeType = gradeType;
+        this.dateTime = dateTime;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Grade{" +
+                "courseUuid=" + courseUuid +
+                ", gradeType=" + gradeType +
+                ", dateTime=" + dateTime +
+                ", value=" + value +
+                '}';
+    }
+}
---
+++ src/main/java/pl/sdacademy/java/exercise/GradeType.java
@@ -0,0 +1,7 @@
+package pl.sdacademy.java.exercise;
+
+public enum GradeType {
+    BOARD,
+    QUIZZ,
+    FINAL;
+}
